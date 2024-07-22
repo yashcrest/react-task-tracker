99f7bb4a290004d9a5fe55e9b5590fc757dc187d
Yash Shrestha <yashcrest@gmail.com>
2024-07-22 22:59:52 +1000

    fix error: tsconfig.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { build, defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { build, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
